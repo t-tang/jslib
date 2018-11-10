c5a9e276eaee6dc192170d383f6ea7e573849f1f
unknown <t-tang@users.noreply.github.com>
2018-11-10 06:59:35 +0000

    clean up option

--- shared/commonjs/option.js
+++ shared/commonjs/option.js
@@ -1,19 +1,17 @@
 
-const evaluate = (thing) => typeof thing === 'function' ? thing() : thing
+const evaluate = (fnOrValue) => typeof fnOrValue === 'function' ? fnOrValue() : fnOrValue
 
 const Option = {
     None: {
-        getOrElse: (els) => evaluate(els),
-        orElse: (els) => evaluate(els),
-        exists: (predicate) => false,
+        getOrElse: (fnOrValue) => evaluate(fnOrValue),
         filter: (predicate) => Option.None,
+        flatMap: (optFn) => Option.None,
         map: (fn) => Option.None
     },
     Some: (value) => ({
-        getOrElse: (els) => value,
-        orElse: (els) => Option.Some(value),
-        exists: (predicate) => predicate(value),
+        getOrElse: (fnOrValue) => value,
         filter: (predicate) => predicate(value) ? Option.Some(value) : Option.None,
+        flatMap: (optFn) => optFn(value),
         map: (fn) => Option.Some(fn(value))
     })
 }
